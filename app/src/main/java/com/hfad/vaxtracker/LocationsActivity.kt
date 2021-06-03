ff1a15df37f36a9943e794dc5d08e192e06aa2f5
Gurskaia <gurskaiaanastasia@gmail.com>
2021-06-03 16:38:45 -0700

    Buttons work2

--- app/src/main/java/com/hfad/vaxtracker/LocationsActivity.kt
+++ app/src/main/java/com/hfad/vaxtracker/LocationsActivity.kt
@@ -1,11 +1,18 @@
 package com.hfad.vaxtracker
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 
 class LocationsActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_locations)
+        val btnBackToFirstPage: Button = findViewById(R.id.newZipButton)
+        btnBackToFirstPage.setOnClickListener {
+            val intent = Intent(this, MainActivity::class.java)
+            startActivity(intent)
+        }
     }
 }
--- app/src/main/java/com/hfad/vaxtracker/MainActivity.kt
+++ app/src/main/java/com/hfad/vaxtracker/MainActivity.kt
@@ -1,11 +1,27 @@
 package com.hfad.vaxtracker
 
+
 import androidx.appcompat.app.AppCompatActivity
+import android.content.Intent
+
 import android.os.Bundle
+import android.widget.Button
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val btnOpenSecondPage: Button = findViewById(R.id.button)
+        btnOpenSecondPage.setOnClickListener {
+            val intent = Intent(this, LocationsActivity::class.java)
+            startActivity(intent)
+        }
+
+        val btnOpenFourthPage: Button = findViewById(R.id.button2)
+        btnOpenFourthPage.setOnClickListener {
+            val intent = Intent(this, LocationsActivity::class.java)
+            startActivity(intent)
+        }
     }
 }
