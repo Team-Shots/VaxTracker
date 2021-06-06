bbb988a9a5c16d62202503e94cfb248448c867cf
unknown <alnourimahmmoud@gmail.com>
2021-06-05 18:55:03 -0700

    added locations list, implemented phone/website fucntionality

--- app/src/main/java/com/hfad/vaxtracker/MainActivity.kt
+++ app/src/main/java/com/hfad/vaxtracker/MainActivity.kt
@@ -6,6 +6,7 @@ import android.content.Intent
 
 import android.os.Bundle
 import android.widget.Button
+import android.widget.EditText
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
