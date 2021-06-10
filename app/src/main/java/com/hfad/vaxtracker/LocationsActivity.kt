36e49256f2095b19ba3b6e5561fee6642b744cc0
xbt23x <47095043+xbt23x@users.noreply.github.com>
2021-06-10 14:22:31 -0700

    pushing code progress to discuss with team

--- app/src/main/java/com/hfad/vaxtracker/LocationsActivity.kt
+++ app/src/main/java/com/hfad/vaxtracker/LocationsActivity.kt
@@ -1,6 +1,8 @@
 package com.hfad.vaxtracker
 
+import android.Manifest
 import android.content.Intent
+import android.content.pm.PackageManager
 import android.location.Location
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
@@ -8,13 +10,15 @@ import android.view.View
 import android.widget.Button
 import android.widget.AdapterView.OnItemClickListener
 import android.widget.ListView
-import com.google.android.gms.location.FusedLocationProviderClient
-import com.google.android.gms.location.LocationRequest
-import com.google.android.gms.location.LocationServices
+import androidx.core.app.ActivityCompat
+import com.google.android.gms.location.*
+import com.google.android.gms.tasks.Task
 
 class LocationsActivity : AppCompatActivity() {
 
+    //Location services
     private lateinit var fusedLocationClient: FusedLocationProviderClient
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
 
@@ -26,7 +30,7 @@ class LocationsActivity : AppCompatActivity() {
         // variable to store results of distance calculation
         var distance = FloatArray(3)
 
-
+        //Location services client
         fusedLocationClient = LocationServices.getFusedLocationProviderClient(this)
 
         super.onCreate(savedInstanceState)
@@ -43,6 +47,29 @@ class LocationsActivity : AppCompatActivity() {
         val listView = findViewById<View>(R.id.list_locations) as ListView
         listView.onItemClickListener = itemClickListener
 
+        //Check here to see if we have permissions
+        if (ActivityCompat.checkSelfPermission(
+                this,
+                Manifest.permission.ACCESS_FINE_LOCATION
+            ) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(
+                this,
+                Manifest.permission.ACCESS_COARSE_LOCATION
+            ) != PackageManager.PERMISSION_GRANTED
+        ) {
+            // TODO: Consider calling
+            //    ActivityCompat#requestPermissions
+            // here to request the missing permissions, and then overriding
+            //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
+            //                                          int[] grantResults)
+            // to handle the case where the user grants the permission. See the documentation
+            // for ActivityCompat#requestPermissions for more details.
+            return
+        }
+        fusedLocationClient.lastLocation
+            .addOnSuccessListener { location : Location? ->
+                // Got last known location. In some rare situations this can be null.
+            }
+
 
         //android.location.Location.distanceBetween(currLat, currLong, locLat, locLang, distance)
 
@@ -52,7 +79,7 @@ class LocationsActivity : AppCompatActivity() {
             startActivity(intent)
         }
 
-        
+
 
         //Create the location request and set the parameters
 
@@ -64,5 +91,24 @@ class LocationsActivity : AppCompatActivity() {
             }
         }
 
+
+
+
+        //Check current location settings
+        val builder = LocationSettingsRequest.Builder()
+
+
+        val client: SettingsClient = LocationServices.getSettingsClient(this)
+        val task: Task<LocationSettingsResponse> = client.checkLocationSettings(builder.build())
+
+
+        task.addOnSuccessListener { locationSettingsResponse ->
+            // All location settings are satisfied. The client can initialize
+            // location requests here.
+            // ...
+        }
+
+
     }
+
 }
